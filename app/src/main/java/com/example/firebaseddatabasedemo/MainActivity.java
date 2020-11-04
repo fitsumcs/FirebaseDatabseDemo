0747e29a87019b62070fd8ad6af2690dd23d0465
Fitsum A <fitsumcs@gmail.com>
2020-11-04 08:35:09 -0900

    Adding Artist To Database

--- app/src/main/java/com/example/firebaseddatabasedemo/MainActivity.java
+++ app/src/main/java/com/example/firebaseddatabasedemo/MainActivity.java
@@ -1,14 +1,101 @@
 package com.example.firebaseddatabasedemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //view objects
+    EditText editTextName;
+    Spinner spinnerGenre;
+    Button buttonAddArtist;
+    ListView listViewArtists;
+
+    //a list to store all the artist from firebase database
+    List<Artist> artists;
+
+    //our database reference object
+    DatabaseReference databaseArtists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getting the reference of artists node
+        databaseArtists = FirebaseDatabase.getInstance().getReference("artists");
+
+
+
+        //getting views
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        spinnerGenre = (Spinner) findViewById(R.id.spinnerGenres);
+        listViewArtists = (ListView) findViewById(R.id.listViewArtists);
+
+        //list to store artists
+        artists = new ArrayList<>();
+
+
     }
+
+    public void addArtist(View view) {
+
+        //getting the values to save
+        String name = editTextName.getText().toString().trim();
+        String genre = spinnerGenre.getSelectedItem().toString();
+
+        //checking if the value is provided
+        if (!TextUtils.isEmpty(name)) {
+
+            //getting a unique id using push().getKey() method
+            String id = databaseArtists.push().getKey();
+
+            //creating an Artist Object
+            Artist artist = new Artist(id, name, genre);
+
+            //Saving the Artist
+            databaseArtists.child(id).setValue(artist).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        //Task was successful, data written!
+                        Toast.makeText(MainActivity.this, "Data saved!", Toast.LENGTH_SHORT).show();
+                    }else{
+                        //Task was not successful,
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                        //Log the error message
+                        Log.e("Error on Databse", "onComplete: ERROR: " + task.getException().getLocalizedMessage() );
+
+                    }
+                }
+            });
+
+            //setting edittext to blank again
+            editTextName.setText("");
+
+
+        } else {
+            //if the value is not given displaying a toast
+            Toast.makeText(this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
